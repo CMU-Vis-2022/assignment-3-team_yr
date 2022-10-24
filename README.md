aadfbff8e585bee26ab06143e79a902d28620b95
yuewang4 <113554330+yuewang4@users.noreply.github.com>
2022-10-24 15:48:07 -0400

    part 2 first attempt

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=8780035&assignment_repo_type=AssignmentRepo)
+
 # D3-App-Template
 
 A template for an interactive web application with D3
--- src/main2.ts
+++ src/main2.ts
@@ -4,26 +4,24 @@ import * as d3 from "d3";
 import { barChart } from "./bar-chart";
 import { Int32, Table, Utf8 } from "apache-arrow";
 import { db } from "./duckdb";
-import parquet from "./weather.parquet?url";
+import parquet from "./pittsburgh-air-quality.parquet?url";
 
 const app = document.querySelector("#app")!;
 
 // Create the chart. The specific code here makes some assumptions that may not hold for you.
 const chart = barChart();
 
+//update the code to sql that get the polluten
 async function update(location: string) {
   // Query DuckDB for the data we want to visualize.
-  const data: Table<{ weather: Utf8; cnt: Int32 }> = await conn.query(`
-  SELECT weather, count(*)::INT as cnt
-  FROM weather.parquet
-  WHERE location = '${location}'
-  GROUP BY weather
-  ORDER BY cnt DESC`);
+  const data: Table<{ pollutant: Utf8; cnt: Int32 }> = await conn.query(`
+  SELECT 'US AQI' pollutant, count()::INT as cnt from "pittsburgh-air-quality.parquet" WHERE "City" = '${location}'
+  `);
 
   // Get the X and Y columns for the chart. Instead of using Parquet, DuckDB, and Arrow, we could also load data from CSV or JSON directly.
   const X = data.getChild("cnt")!.toArray();
   const Y = data
-    .getChild("weather")!
+    .getChild("pollutant")!
     .toJSON()
     .map((d) => `${d}`);
 
@@ -33,31 +31,34 @@ async function update(location: string) {
 // Load a Parquet file and register it with DuckDB. We could request the data from a URL instead.
 const res = await fetch(parquet);
 await db.registerFileBuffer(
-  "weather.parquet",
+  "pittsburgh-air-quality.parquet", //replace with my file
   new Uint8Array(await res.arrayBuffer())
 );
 
 // Query DuckDB for the locations.
 const conn = await db.connect();
 
+console.log("Running here");
+//replace location with city and the data
 const locations: Table<{ location: Utf8 }> = await conn.query(`
-SELECT DISTINCT location
-FROM weather.parquet`);
+SELECT DISTINCT City
+FROM "pittsburgh-air-quality.parquet"`);
+
+console.log(locations);
 
 // Create a select element for the locations.
 const select = d3.select(app).append("select");
 for (const location of locations) {
-  select.append("option").text(location.location);
+  select.append("option").text(location.City);
 }
 
 select.on("change", async () => {
   const location = select.property("value");
-
   update(location);
 });
 
 // Update the chart with the first location.
-update("Seattle");
+update("Avalon");
 
 // Add the chart to the DOM.
 app.appendChild(chart.element);
